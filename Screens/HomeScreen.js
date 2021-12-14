d903bd785222869af549aecb20b54e81ff1c000c
rishav-raj1 <rishavkumarraj801@gmail.com>
2021-12-14 23:48:44 +0530

    Navigation

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,10 +1,15 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>I Am The Home Screen</Text>
+      <Button
+        title="Go to Chat Screen"
+        onPress={() => navigation.navigate('Chat')}></Button>
     </View>
   );
 };
--- StackNavigator.js
+++ StackNavigator.js
@@ -2,12 +2,14 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {View, Text} from 'react-native';
 import HomeScreen from './Screens/HomeScreen';
+import ChatScreen from './Screens/ChatScreen';
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
     </Stack.Navigator>
   );
 };
