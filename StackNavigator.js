4019c119e361a725db2c67cc4efbec6f812a8e3b
rishav-raj1 <rishavkumarraj801@gmail.com>
2021-12-14 23:59:09 +0530

    Login Screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import HomeScreen from './Screens/HomeScreen';
 import ChatScreen from './Screens/ChatScreen';
+import LoginScreen from './Screens/LoginScreen';
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
@@ -10,6 +11,7 @@ const StackNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 };
